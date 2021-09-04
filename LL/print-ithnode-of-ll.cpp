544d2e6f31e0d251eb226ee5f8da6f980ec2e549
Akshat Jain <akshatjain384@gmail.com>
2021-09-05 03:39:14 +0530

    added print ith node program

---
+++ LL/print-ithnode-of-ll.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+#include "solution.h"
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newNode = new Node(data);
+		if (head == NULL)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+void printIthNode(Node *head, int i)
+{
+    //Write your code here
+    int j=0;
+    Node *temp=head;
+    
+    while(temp!=NULL)
+    {       
+           if(j==i)
+        {
+            cout<<temp->data;
+             break;
+        }
+        else{
+        temp=temp->next;
+            j++;
+       }
+    }
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		int pos;
+		cin >> pos;
+		printIthNode(head, pos);
+		cout << endl;
+	}
+	return 0;
+}
