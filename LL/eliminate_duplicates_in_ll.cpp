d1e1d493aa3fcc75d475c060fdc4c41e5e8e6df7
Akshat Jain <akshatjain384@gmail.com>
2021-11-07 00:11:53 +0530

    added eliminate duplicate elements in a linked list program

---
+++ LL/eliminate_duplicates_in_ll.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+Node *removeDuplicates(Node *head)
+{
+    //Write your code here
+    if(head==NULL)
+    {
+        return head;
+    }
+    Node *p=head;
+    Node *q=head->next;
+    while(q!=NULL){
+    if(p->data!=q->data)
+    {
+        p=q;
+        q=q->next;
+    }
+        else if(p->data==q->data)
+        {
+            p->next=q->next;
+            q=q->next;
+        }
+    }
+    return head;
+}
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newnode = new Node(data);
+		if (head == NULL)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail->next = newnode;
+			tail = newnode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+void print(Node *head)
+{
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		head = removeDuplicates(head);
+		print(head);
+	}
+	return 0;
+}
