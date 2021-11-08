f3121c3e7be1335eee44fac9aedfa8a72b25d284
Akshat Jain <akshatjain384@gmail.com>
2021-11-09 00:00:20 +0530

    added reverse a link list using recursion program

---
+++ LL/LL using recursion/reverse_LL_using_recursion.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+Node *reverseLinkedListRec(Node *head)
+{
+    //Write your code here
+    if (head==NULL or head->next==NULL)
+    {
+        return head;
+    }
+    Node* temp=head;
+    head=reverseLinkedListRec(head->next);
+    Node*temp2=head;
+    while(temp2->next!=NULL)
+    {
+        temp2=temp2->next;
+    }
+    temp2->next=temp;
+    temp->next=NULL;
+    return head;
+}
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newnode = new Node(data);
+		if (head == NULL)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail->next = newnode;
+			tail = newnode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+void print(Node *head)
+{
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+
+	while(t--)
+	{
+		Node *head = takeinput();
+		head = reverseLinkedListRec(head);
+		print(head);
+	}
+
+	return 0;
+}
