4fac6e57bac441ffaffaf3b9d1495076c96de24b
Akshat Jain <akshatjain384@gmail.com>
2021-11-21 16:57:22 +0530

    added how to implement inbuilt stacks

---
+++ stacks/inbuilt_stack.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<stack>
+using namespace std;
+int main()
+{
+    stack <int>s1;
+    s1.push(20);
+    s1.push(30);
+    s1.push(40);
+    cout<<s1.top()<<endl;
+    s1.pop();
+    cout<<s1.top()<<endl;
+    cout<<s1.empty()<<endl;
+    cout<<s1.size();
+
+    return 0;
+}
