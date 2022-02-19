fa9ec8317502dce7b447e2d9182049292a9a212e
Akshat Jain <akshatjain384@gmail.com>
2022-02-19 15:51:50 +0530

    modified

--- My_Notes/Iterator_notes_hashmaps.cpp
+++ My_Notes/Iterator_notes_hashmaps.cpp
@@ -20,3 +20,28 @@ int main()
       }
     return 0;
 }
+
+//for ordered maps 
+/*
+#include <iostream>
+#include<map>
+#include<string>
+using namespace std;
+
+int main()
+{
+  
+    map<string,int>mymap;
+    mymap["abc"]=1;
+    mymap["def"]=2;
+    mymap["ghi"]=3;
+
+      //Iterators are used to iterate through all the keys in the map 
+      map<string,int>::iterator it =mymap.begin(); //initialising iterator 
+      while(it!=mymap.end())
+      {
+          cout<<"Key:"<<it->first<<"  "<<"Value:"<<it->second<<endl; //it->first gives key ; it->second gives value 
+              it++;                                                       //corresponding to key
+      }
+    return 0;
+}*/
