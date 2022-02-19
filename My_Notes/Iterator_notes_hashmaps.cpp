006197260f5b423f509e18218253c37b371dda98
Akshat Jain <akshatjain384@gmail.com>
2022-02-19 14:44:28 +0530

    added iterator notes

---
+++ My_Notes/Iterator_notes_hashmaps.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<unordered_map>
+#include<string>
+using namespace std;
+
+int main()
+{
+  
+    unordered_map<string,int>mymap;
+    mymap["abc"]=1;
+    mymap["def"]=2;
+    mymap["ghi"]=3;
+
+      //Iterators are used to iterate through all the keys in the map 
+      unordered_map<string,int>::iterator it =mymap.begin(); //initialising iterator 
+      while(it!=mymap.end())
+      {
+          cout<<"Key:"<<it->first<<"  "<<"Value:"<<it->second<<endl; //it->first gives key ; it->second gives value 
+              it++;                                                       //corresponding to key
+      }
+    return 0;
+}
