b9c5ba4f71522a1308dc8038c9e746ca38bc1b66
Akshat Jain <akshatjain384@gmail.com>
2022-02-24 20:41:07 +0530

    added traversals

---
+++ BinaryTrees/Traversal/inorder.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+template <typename T>
+class BinaryTreeNode {
+   public:
+    T data;
+    BinaryTreeNode<T>* left;
+    BinaryTreeNode<T>* right;
+
+    BinaryTreeNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void inorder(BinaryTreeNode<int>*root)
+{
+	if(root==NULL)
+	{
+		return;
+	}
+	inorder(root->left);
+	cout<<root->data<<' ';
+	inorder(root->right);
+}
+
+
+BinaryTreeNode<int>* takeInput() {
+    int rootData;
+    cin >> rootData;
+    if (rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int>* root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    while (!q.empty()) {
+        BinaryTreeNode<int>* currentNode = q.front();
+        q.pop();
+        int leftChild, rightChild;
+
+        cin >> leftChild;
+        if (leftChild != -1) {
+            BinaryTreeNode<int>* leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode->left = leftNode;
+            q.push(leftNode);
+        }
+
+        cin >> rightChild;
+        if (rightChild != -1) {
+            BinaryTreeNode<int>* rightNode =
+                new BinaryTreeNode<int>(rightChild);
+            currentNode->right = rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+int main() {
+	
+    BinaryTreeNode<int>* root = takeInput();
+    inorder(root);
+}
