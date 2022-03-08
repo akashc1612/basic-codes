f604b822449f4755888a06ddc60ab75166c4a048
Akshat Jain <akshatjain384@gmail.com>
2022-03-08 17:11:00 +0530

    added printing tree in zig zag fashion program

---
+++ BST/adv/zig_zag_print_of_btree.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+template <typename T>
+class BinaryTreeNode {
+   public:
+    T data;
+    BinaryTreeNode<T>* left;
+    BinaryTreeNode<T>* right;
+
+    BinaryTreeNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+ void print( vector<BinaryTreeNode<int> *>v)
+    {
+        for(int i=0;i<v.size();i++)
+        {
+            cout<<v[i]->data<<' ';
+        }
+        cout<<endl;
+    }
+
+void zigZagOrder(BinaryTreeNode<int> *root) {
+    // Write your code here
+    if(root==NULL)
+    {
+        return;
+    }
+    int count=0;
+    queue<BinaryTreeNode<int> *>q;
+    q.push(root);
+    while(1)
+    {
+        int currsize=q.size();
+        vector<BinaryTreeNode<int> *>v;
+        if (currsize==0)
+        {
+            break;
+        }
+        while(currsize>0)
+        {
+          
+                BinaryTreeNode<int> *node=q.front();
+                v.push_back(node);
+                if(node->left!=NULL)
+                {
+                    q.push(node->left);
+                }
+                if(node->right!=NULL)
+                {
+                    q.push(node->right);
+                }
+               q.pop();
+            currsize--;
+        }
+        if(count%2!=0)
+            {
+               int si=0;
+                int ei=v.size()-1;
+                while(si<=ei)
+                {
+                    BinaryTreeNode<int> *temp=v[si];
+                    v[si]=v[ei];
+                    v[ei]=temp;
+                    si++;
+                    ei--;
+                }
+                print(v);
+            }
+            if(count%2==0)
+            {
+             print(v);   
+            }
+        count++;
+    }
+}
+
+
+BinaryTreeNode<int>* takeInput() {
+    int rootData;
+    cin >> rootData;
+    if (rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int>* root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    while (!q.empty()) {
+        BinaryTreeNode<int>* currentNode = q.front();
+        q.pop();
+        int leftChild, rightChild;
+
+        cin >> leftChild;
+        if (leftChild != -1) {
+            BinaryTreeNode<int>* leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode->left = leftNode;
+            q.push(leftNode);
+        }
+
+        cin >> rightChild;
+        if (rightChild != -1) {
+            BinaryTreeNode<int>* rightNode =
+                new BinaryTreeNode<int>(rightChild);
+            currentNode->right = rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+int main() {
+    BinaryTreeNode<int>* root = takeInput();
+    zigZagOrder(root);
+}
