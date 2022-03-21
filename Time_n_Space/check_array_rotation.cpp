3dce51698884e0394fbdf0a6d4f1163afdcf6304
Akshat Jain <akshatjain384@gmail.com>
2022-03-22 02:16:16 +0530

    added check array rotation soln

---
+++ Time_n_Space/check_array_rotation.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int arrayRotateCheck(int *input, int size)
+{
+    //Write your code here
+    int index=0;
+    for(int i=0;i<size-1;i++)
+    {
+        if(input[i]>input[i+1])
+        {
+            index=i;
+            break;
+        }
+    }
+    if(index)
+    {
+        return index+1;
+    }
+    return 0;
+}
+
+int main()
+{
+
+	int t;
+	cin >> t;
+	while (t--)
+	{
+
+		int size;
+		cin >> size;
+		int *input = new int[size];
+
+		for (int i = 0; i < size; i++)
+		{
+			cin >> input[i];
+		}
+
+		cout << arrayRotateCheck(input, size) << endl;
+		delete[] input;
+	}
+	
+	return 0;
+}
