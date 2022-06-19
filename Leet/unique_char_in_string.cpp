18f66ae1b24c02e390c8ea9b3b989aa24da51b5e
Akshat Jain <akshatjain384@gmail.com>
2022-06-19 13:11:54 +0530

    added o(n) soln

---
+++ Leet/unique_char_in_string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int arr[26]={0};
+        int len=s.size();
+        for(int i=0;i<len;i++)
+            arr[s[i]-'a']++;
+        for(int i=0;i<len;i++)
+        {
+            if(arr[s[i]-'a']==1)
+                return i;
+        }
+        return -1;
+    }
+};
